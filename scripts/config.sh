3e5c862568e170f43f5c2c63d4ab24c0ca36c2f7
pzeadrian <pze.adrian@gmail.com>
2024-04-24 15:28:58 -0500

    updates in config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 
-if [ -d "$HOME/Documents/" ]; then
+res=$1
 
-    echo "English..."
-    cd $HOME/Documents/.arch/
-    git pull
-    cd
-    mkdir -p $HOME/.config
-    mkdir -p $HOME/.wallpaper
+if [ "$res" = "1080" | "$res" = "768" ] ; then
+    if [ -d "$HOME/Documents/" ]; then
 
-    cp -rf $HOME/Documents/.arch/apps/* $HOME/.config/
-    cp -f $HOME/Documents/.arch/apps/.bashrc $HOME/
-    cp -rf $HOME/Documents/.arch/wallpaper/* $HOME/.wallpaper/
+        echo "English..."
+        cd $HOME/Documents/.arch/
+        git pull
+        cd
+        mkdir -p $HOME/.config
+        mkdir -p $HOME/.wallpaper
 
-    read -p "RESOLUTION (1080 or 768): " res
-    sh $HOME/Documents/.arch/scripts/resolution $res
+        cp -rf $HOME/Documents/.arch/apps/* $HOME/.config/
+        cp -f $HOME/Documents/.arch/apps/.bashrc $HOME/
+        cp -rf $HOME/Documents/.arch/wallpaper/* $HOME/.wallpaper/
 
-elif [ -d "$HOME/Documentos/" ]; then
-    echo "Spanish..."
+        sh $HOME/Documents/.arch/scripts/resolution $res
 
-    cd $HOME/Documentos/.arch/
-    git pull
-    cd
-    mkdir -p $HOME/.config
-    mkdir -p $HOME/.wallpaper
+    elif [ -d "$HOME/Documentos/" ]; then
+        echo "Spanish..."
 
-    cp -rf $HOME/Documentos/.arch/apps/* $HOME/.config/
-    cp -f $HOME/Documentos/.arch/apps/.bashrc $HOME/
-    cp -rf $HOME/Documentos/.arch/wallpaper/* $HOME/.wallpaper/
+        cd $HOME/Documentos/.arch/
+        git pull
+        cd
+        mkdir -p $HOME/.config
+        mkdir -p $HOME/.wallpaper
 
-    read -p "RESOLUTION (1080 or 768): " res
-    sh $HOME/Documentos/.arch/scripts/resolution $res
+        cp -rf $HOME/Documentos/.arch/apps/* $HOME/.config/
+        cp -f $HOME/Documentos/.arch/apps/.bashrc $HOME/
+        cp -rf $HOME/Documentos/.arch/wallpaper/* $HOME/.wallpaper/
 
-else 
-    echo "No english or spanish detected..."
+        read -p "RESOLUTION (1080 or 768): " res
+        sh $HOME/Documentos/.arch/scripts/resolution $res
 
+    else 
+        echo "No english or spanish detected..."
+
+    fi
+else
+    echo "Don't forget to add resolution, 1080 or 768 are available rn..."
+    exit
 fi

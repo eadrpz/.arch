4bc0cd64dfe6090eb7f4d28bb86f223c2900a5d9
eadrpz <eadrpz@duck.com>
2024-09-09 07:44:50 -0500

    up in english scripts

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,11 +3,9 @@
 res=$1
 
 if [[ "$res" = 1080 || "$res" = 768 ]] ; then
-    if [ -d "$HOME/Documents/" ]; then
-
         echo "English..."
         cd $HOME/Documents/.arch/
-        git pull
+        git pull origin main
         cd
         mkdir -p $HOME/.config
 
@@ -15,24 +13,6 @@ if [[ "$res" = 1080 || "$res" = 768 ]] ; then
         cp -f $HOME/Documents/.arch/apps/.bashrc $HOME/
 
         sh $HOME/Documents/.arch/scripts/resolution $res
-
-    elif [ -d "$HOME/Documentos/" ]; then
-        echo "Spanish..."
-
-        cd $HOME/Documentos/.arch/
-        git pull
-        cd
-        mkdir -p $HOME/.config
-
-        cp -rf $HOME/Documentos/.arch/apps/* $HOME/.config/
-        cp -f $HOME/Documentos/.arch/apps/.bashrc $HOME/
-
-        sh $HOME/Documentos/.arch/scripts/resolution $res
-
-    else 
-        echo "No english or spanish detected..."
-
-    fi
 else
     echo "Don't forget to add resolution, 1080 or 768 are available rn..."
     exit
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-if [ -d "$HOME/Documents/" ]; then
-
     echo "English..."
     cd $HOME/Documents/.arch/
-    git pull
+    git pull origin main
     cd
     rm -r $HOME/Documents/.arch/apps/*
     # 
@@ -38,49 +36,4 @@ if [ -d "$HOME/Documents/" ]; then
     cd $HOME/Documents/.arch/
     git add -A
     git commit -m "$1"
-    git push
-
-elif [ -d "$HOME/Documentos/" ]; then
-    echo "Spanish..."
-
-    cd $HOME/Documentos/.arch/
-    git pull
-    cd
-    rm -r $HOME/Documentos/.arch/apps/*
-    # 
-    # Bash
-    cp -f $HOME/.bashrc $HOME/Documentos/.arch/apps/
-    # 
-    # Alacritty
-    cp -rf $HOME/.config/alacritty/ $HOME/Documentos/.arch/apps/
-    #
-    # Dunst
-    cp -rf $HOME/.config/dunst/ $HOME/Documentos/.arch/apps/
-    #
-    # MPV
-    cp -rf $HOME/.config/mpv/ $HOME/Documentos/.arch/apps/
-    #
-    # Rofi
-    cp -rf $HOME/.config/rofi/ $HOME/Documentos/.arch/apps/
-    #
-    # Hyprland
-    cp -rf $HOME/.config/hypr/ $HOME/Documentos/.arch/apps/
-    #
-    # Waybar
-    cp -rf $HOME/.config/waybar/ $HOME/Documentos/.arch/apps/
-    #
-    # Swaylock
-    # cp -rf $HOME/.config/swaylock $HOME/Documentos/.arch/apps/
-    #
-    # Cava
-    cp -rf $HOME/.config/cava $HOME/Documentos/.arch/apps/
-    #
-    cd $HOME/Documentos/.arch/
-    git add -A
-    git commit -m "$1"
-    git push
-
-else 
-    echo "No english or spanish detected..."
-
-fi
+    git push origin main
